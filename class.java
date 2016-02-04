729d61df31df2fa3d843d78ac36f996f6aee8f0b
Tim Urista <timothy.urista@gmail.com>
2016-02-04 10:56:28 -0700

    Filled out the class header
    Fixed name of the file and filled out the class

--- class.java
+++
@@ -1,3 +0,0 @@
-// TODO: replace the greeting below with your own greeting
-System.out.println("Hello world!");
-// TODO: print a second greeting to the user
---
+++ myClass.java
@@ -0,0 +1,8 @@
+class myClass {
+
+  public static void main(String[] args) {
+  // TODO: replace the greeting below with your own greeting
+  System.out.println("Hello world!");
+  // TODO: print a second greeting to the user
+  }
+}
